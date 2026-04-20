7bfb8fe9f30924270e78ac34b9af7386a1b1b64e
kkkorkor <kwankaow2112@gmail.com>
2026-04-20 07:52:14 +0000

    .

--- main.cpp
+++ main.cpp
@@ -1,18 +1,50 @@
-int main(){
-	int x,y;
-	double d;
-	cin>>x>>y;
-	if(cin.fail()){
-		cerr<<"Incorrect type entered"; return 1;
-	}
-	if(abs(x)>1000 ||abs(y)>1000){
-		cerr<<"alue out of range"; return 1;
-	}
-	if (y==0) {
-		cerr<< "Error divide by zero"; return 1;
-	}
+#include<iostream>
+using namespace std;
 
-	d=(double) x/y;
-	cout<< "The result is" <<d;
+void check0(int);
+
+int main(){
+	int x,y,a;
+	double d;
+	do{
+		a=0;
+		try{
+		cout<<"Enter 2 Numbers: ";
+		cin>>x>>y;
+
+		//Curated Exception Handling
+		if(cin.fail()) throw 101; //throw string
+		if(abs(x)>1000 ||abs(y)>1000) throw "Value out of range"; //throw string
+		//if(y==0) throw "Error divide by zero";
+		check0(y);
+
+		/*Normal Code*/
+		d=(double) x/y;
+		cout<< "The result is " <<d<<endl;
+	
+		}
+		catch(const char* error){ /*ถ้าไม่มี const compile ไม่ผ่านเพราะ throw เป็น string*/
+			cerr<<error<<endl;
+			a=1;
+			cin.clear();
+			cin.ignore(50,'\n');
+		}
+		catch(int e){
+			switch(e){
+				case 101: cerr<<"Incorrect numbers "<<endl; break;
+				case 0: cerr<< "Error divide by zero" <<endl; break;;
+			}
+			a=1;
+			cin.clear();
+			cin.ignore(50,'\n');
+
+		}
+
+	}while(a);
+	//catch(int x)
 	return 0;
+}
+
+void check0(int x){
+	if(x==0) throw(x);
 }
